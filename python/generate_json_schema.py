3edd7a7d6912f9e4ec028edfd6ee2907855879fe
li-linfang <93443881+li-linfang@users.noreply.github.com>
2022-05-06 14:26:38 +0800

    version1

--- python/generate_json_schema.py
+++ python/generate_json_schema.py
@@ -1,5 +1,6 @@
 from docx import Document
 import json
+import os
 
 
 document = Document("D:/0-DesktopData/python-mqtt-client/docx/mqtt.docx")
@@ -7,12 +8,84 @@ document = Document("D:/0-DesktopData/python-mqtt-client/docx/mqtt.docx")
 tables = document.tables
 
 
-schema_type_dic = {"字符串":"string", "整型": "number", "对象": "object", "数组": "array", "布尔": "boolean", "空": "null"}
+schema_type_dic = {"字符串":"string", "整型": "integer", "对象": "object", "数组": "array", "布尔": "boolean", "空": "null", "浮点型": "number"}
 
 
-def generate_new_xlsx_file(table, count):
+def get_cur_parent_node(the_json, level, parent_nodes):
+    '''获得当前父级节点'''
+    if level == 0:
+        return the_json
+    elif level == 1:
+        return the_json['properties'][parent_nodes[0]]
+    elif level == 2:
+        return the_json['properties'][parent_nodes[0]]['properties'][parent_nodes[1]]
+    elif level == 3:
+        return the_json['properties'][parent_nodes[0]]['properties'][parent_nodes[1]]['properties'][parent_nodes[2]]
+    else:
+        return "false"
+
+
+
+def deal_with(i, table, the_json, level, parent_nodes):
+    '''处理数据'''
+
+    name = table.cell(i, 0).text
+    real_name = name.replace('+', '')
+    definition = table.cell(i, 1).text
+    required = table.cell(i, 2).text
+    restriction = table.cell(i, 3).text
+    the_type = table.cell(i, 4).text.replace(' ', '')
+    description = table.cell(i, 5).text
+
+
+    print("---------------------------------------------------------------------------------------------------------------")
+    # print(the_json['required'])
+    print(real_name + " - " + str(level))
+    print(parent_nodes)
+    cur_parent_node = get_cur_parent_node(the_json, level, parent_nodes)
+
+
+
+    msg = ""
+    if restriction != "":
+        msg = "取值范围：" + restriction +  "；"
+    if description != "":
+        msg += description
+
+
+    if the_type == "JSON对象":
+        the_type = "对象"
+        parent_nodes[level] = real_name
+        # print("--------> 2 <-------")
+        cur_parent_node['properties'][real_name] = { "type": schema_type_dic[the_type], "title": definition, "description":  msg}
+        cur_parent_node['properties'][real_name]['properties'] = {}
+        cur_parent_node['properties'][real_name]['required'] = []
+    elif the_type == "JSON数组":
+        # 这里其实是一个 json 对象数组
+        # the_type = "数组"
+        # parent_nodes[level] = real_name
+        # cur_parent_node['properties'][real_name] = { "type": schema_type_dic[the_type], "title": definition, "description":  msg}
+        # cur_parent_node['properties'][real_name]['items'] = {"type": "object"}
+    elif the_type == "JSON字符串数组":
+        the_type = "数组"
+        cur_parent_node['properties'][real_name] = { "type": schema_type_dic[the_type], "title": definition, "description":  msg}
+        cur_parent_node['properties'][real_name]['items'] = {"type": "string"}
+    elif the_type == "JSON整型数组":
+        the_type = "数组"
+        cur_parent_node['properties'][real_name] = { "type": schema_type_dic[the_type], "title": definition, "description":  msg}
+        cur_parent_node['properties'][real_name]['items'] = {"type": "number"}
+    else:
+        cur_parent_node['properties'][real_name] = { "type": schema_type_dic[the_type], "title": definition, "description":  msg}
+
+    if required == "Y":
+        required_list = cur_parent_node['required']
+        required_list.append(real_name)
+        cur_parent_node['required'] = required_list
+        print("##################################")
+        print(cur_parent_node['required'])
+
+def generate_new_xlsx_file(table, num):
     """生成一个新的 xlsx 文件"""
-
     # 名称	    定义	            必需	长度/范围	    类型	        备注
     # Action	固定“addPerson”	    Y		                字符串	
     
@@ -25,61 +98,32 @@ def generate_new_xlsx_file(table, count):
     the_json['type'] = 'object'
     the_json['properties'] = {}
     the_json['required'] = []
-    required_list = []
     parent_nodes = ["", "", "", "", "", "", "", "", "", ""]
 
     for i in range(1, len_rows):
-        # 获得所有字段
         name = table.cell(i, 0).text
-        definition = table.cell(i, 1).text
-        required = table.cell(i, 2).text
-        restriction = table.cell(i, 3).text
-        the_type = table.cell(i, 4).text
-        description = table.cell(i, 5).text
-
         real_name = name.replace('+', '')
+        required = table.cell(i, 2).text
+        # 层级。值为 0 时代表顶层，值为 1 时代表子第一层
+        level = name.count('+')
+
+        if name != "":
+            deal_with(i, table, the_json, level, parent_nodes)
 
 
-
-        if "数组" in the_type:
-            parent_nodes[name.count('+')] = real_name
-            if the_type == "JSON数组":
-                the_type = "对象"
-                the_json['properties'][parent_nodes[0]] = { "type": schema_type_dic[the_type], "title": definition, "description":  description}
-                the_json['properties'][parent_nodes[0]]['properties'] = {}
-            elif the_type == "JSON字符串数组":
-                the_type = "数组"
-                #the_json['properties'][parent_nodes[0]] = { "type": schema_type_dic[the_type], "title": definition, "description":  description}
-                # the_json['properties'][parent_nodes[0]]['items'] = {"type": "string"}
-            elif the_type == "JSON整型数组":
-                the_type = "数组"
-        else:
-            item =  { "type": schema_type_dic[the_type], "title": definition, "description":  description}
-            the_json['properties'][name] = item
-        # 如果是属于某一个对象中的元素，则进行不同的操作
-        if "+" in name:
-            item =  { "type": schema_type_dic[the_type], "title": definition, "description":  description}
-            # 统计 + 符号的个数，用于计算正在处于第几层。有 1 个说明位于下面一层，有 2 个说明位于下面 2 层
-            levels = name.count('+')
-            _name = real_name
-            # 有多少层，就深入到多少层的 json 语句中去，进行赋值
-            if levels == 1:
-                print(name)
-                print(parent_nodes)
-                the_json['properties'][parent_nodes[0]]['properties'][_name] = item
-
-
-        if required == "Y":
-            required_list.append(real_name)
-
-    the_json['required'] = required_list
+    action_name = table.cell(1, 1).text
     print(the_json)
+    write_json_schema_file(num, action_name, the_json)
 
 
+def write_json_schema_file(num, action_name, the_json):
+    ''' 写入文件 '''
+    with open("json_schema_collections/"+str(num)+"_"+action_name+".json","w") as f:
+        json.dump(the_json,f)
 
+if not os.path.exists('json_schema_collections'):
+    os.mkdir('json_schema_collections')
 
-
-    # print(f'Action: {action},  len_rows: {len_rows},  len_columns: {len_columns}')
 
 tables_count = len(tables)
 print(tables_count)
@@ -89,6 +133,6 @@ for i in range(tables_count):
     if tables[i].cell(1,0).text == "Action" and len(tables[i].columns) == 6:
         count += 1
         generate_new_xlsx_file(tables[i], count)
-        break
+        # break
 print(count)
 
