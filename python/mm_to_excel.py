b423cec96f481b3d0ed005772553c7922bf29444
storeview <93443881+storeview@users.noreply.github.com>
2023-02-23 16:54:18 +0800

    v1.0.6.0     输入0, 代表将根节点下一级中所有包含测试用例的子节点作为起点. 建立多个sheet

--- python/mm_to_excel.py
+++ python/mm_to_excel.py
@@ -21,6 +21,8 @@ from openpyxl.styles import Alignment,Border, Side, Font, PatternFill
 2023-2-23
 v1.0.5.0
     添加测试用例优先级字段.
+v1.0.6.0
+    输入0, 代表将根节点下一级中所有包含测试用例的子节点作为起点. 建立多个sheet
 """
 
 # 使用文件作为解析对象
@@ -34,7 +36,7 @@ root = tree.getroot()
 # root.tag
 
 print("#"*60)
-sheet_name = input("请输入测试用例生成的开始节点（该节点将作为工作蒲sheet的表名，下一级将作为【测试主模块】，若不输入任何值则以思维导图的根节点为起点）:\n")
+sheet_name = input("请输入测试用例生成的开始节点（该节点将作为工作蒲sheet的表名，下一级将作为【测试主模块】，若不输入任何值则以思维导图的根节点为起点.  输入0则将根节点下一级中所有包含测试用例的子节点作为起点）:\n")
 # 选择测试用例生成的节点，该节点的下一级将作为【测试主模块】，该节点的名称将作为工作蒲 sheet 的表名
     #|-指定节点
     #|__test_main_module
@@ -53,137 +55,155 @@ if top_node == None:
 
 
 
+
+
+
+
+
+def create_testcase_sheet(sheet, top_node):
+    '''创建测试用例表格'''
+
+    # 设置表格样式（列宽、表头）
+    sheet.column_dimensions['A'].width=5
+    sheet.column_dimensions['B'].width=20
+    sheet.column_dimensions['C'].width=20
+    sheet.column_dimensions['D'].width=25
+    sheet.column_dimensions['E'].width=25
+    sheet.column_dimensions['F'].width=50
+    sheet.column_dimensions['G'].width=25
+    sheet.column_dimensions['H'].width=10
+    sheet.column_dimensions['I'].width=25
+    sheet.column_dimensions['J'].width=25
+
+    sheet.append(["编号", "测试主模块", "测试模块", "测试内容", "前置条件", "测试步骤", "预期结果", "测试结果", "备注", "异常情况说明", "优先级"])
+
+    # 解析mm文件, 并生成表格(代码可以进一步优化)
+
+    def deal_mm_file(node, level):
+        if level == 4:
+            level = level + 1
+        text = node.get("TEXT")
+
+        if text[0:2] == "tp":
+            print("--------------------> 前置条件:\n " + text)
+            one_row[4] = text[3:]
+            return
+        elif text[0:2] == "tr":
+            print("--------------------> 备注:\n " + text)
+            one_row[8] = text[3:]
+            return
+        elif text[0:2] == "te":
+            print("--------------------> 异常情况说明:\n " + text)
+            one_row[9] = text[3:]
+            return
+        elif len(text) == 2 and text[0:1] == "P":
+            print("--------------------> 优先级:\n " + text)
+            one_row[10] = text
+            return 
+        else:
+            print("--------------------> level "+str(level)+" :\n " + text)
+            one_row[level] = text
+
+        # 递归下一层级
+        for sub_node in node:
+            deal_mm_file(sub_node, level+1)
+        # 添加一行表格
+        if level == 6:
+            addNewNode2()
+            # 清空最后三行的数据
+            one_row[7] = ''
+            one_row[8] = ''
+            one_row[9] = ''
+            one_row[10] = ''
+
+        # 清空本次递归填写的数据
+        if level != 7:
+            one_row[level] = ''
+
+    def addNewNode2():
+        print("--------------------> 正在输出一行表格 <--------------------")
+        sheet.append([one_row[0].strip(), one_row[1].strip(), one_row[2].strip(), one_row[3].strip(), one_row[4].strip(), "\n"+one_row[5].strip()+"\n", one_row[6].strip(), one_row[7].strip(), one_row[8].strip(), one_row[9].strip(), one_row[10].strip()])
+
+    # 将上面复杂的代码, 转换为一个递归进行处理
+    # 递归中包含这样的逻辑: 
+    #   当此节点的子节点长度为0时,代表此节点是最后一个了, 此时结束循环.
+
+    one_row = ['', '', '', '', '', '', '', '', '', '', '']
+    # 测试主模块
+    for test_main_module in top_node:
+        if test_main_module.tag != "node":
+            continue
+        deal_mm_file(test_main_module, 1)
+        # 清空
+        one_row = ['', '', '', '', '', '', '', '', '', '', '']
+
+    # 表格生成完成，进行格式处理
+    thin = Side(border_style="thin", color="000000")
+
+    i = 1
+    # 对所有单元格进行处理(横排竖排对齐, 以及边框)
+    while sheet.cell(i, 1).value != None:
+        for j in range(1, 12):
+            if i== 1:
+                # 上下左右居中对齐，自动换行
+                sheet.cell(i, j).alignment = Alignment(horizontal='center', vertical='center', wrapText=True)
+            elif j == 1 or j == 2 or j == 3 or j == 4:
+                sheet.cell(i, j).alignment = Alignment(horizontal='center', vertical='center', wrapText=True)
+            else:
+                sheet.cell(i, j).alignment = Alignment(horizontal='left', vertical='center', wrapText=True)
+            # 边框
+            sheet.cell(i, j).border = Border(top=thin, left=thin, right=thin, bottom=thin)
+        i += 1
+
+    # 设置背景颜色和加粗
+    k = 1
+    ft = Font(bold=True)
+    while sheet.cell(1, k).value != None:
+        sheet.cell(1, k).font = ft
+        sheet.cell(1, k).fill = PatternFill("solid", fgColor="5b9bd5")
+        k+=1
+
+    # 对2-5列的表格进行相邻行内容相同归并处理
+    for col in range (2, 6):
+        row = 2
+        start_row = 2
+        end_row = start_row
+        start_value = sheet.cell(row, col).value 
+        #if start_value == None or start_value == "":
+        #    continue
+        while sheet.cell(row+1, col).value != None:
+            print(str(row+1) +", " + str(col))
+            if sheet.cell(row + 1, col).value == start_value and start_value != "":
+                end_row = row + 1
+            else:
+                sheet.merge_cells(start_row=start_row, start_column=col, end_row=end_row, end_column=col)
+                start_row = row + 1
+                end_row = start_row
+                start_value = sheet.cell(start_row, col).value
+            row += 1
+        sheet.merge_cells(start_row=start_row, start_column=col, end_row=end_row, end_column=col)
+
+
+#create_testcase_sheet(sheet, top_node)
 # 新建工作蒲
 wb = Workbook()
-sheet = wb.active
-sheet.title = top_node.get("TEXT")
-
-# 设置表格样式（列宽、表头）
-sheet.column_dimensions['A'].width=5
-sheet.column_dimensions['B'].width=20
-sheet.column_dimensions['C'].width=20
-sheet.column_dimensions['D'].width=25
-sheet.column_dimensions['E'].width=25
-sheet.column_dimensions['F'].width=50
-sheet.column_dimensions['G'].width=25
-sheet.column_dimensions['H'].width=10
-sheet.column_dimensions['I'].width=25
-sheet.column_dimensions['J'].width=25
 
 
-sheet.append(["编号", "测试主模块", "测试模块", "测试内容", "前置条件", "测试步骤", "预期结果", "测试结果", "备注", "异常情况说明", "优先级"])
-
-
-# 解析mm文件, 并生成表格(代码可以进一步优化)
-
-def deal_mm_file(node, level):
-    if level == 4:
-        level = level + 1
-    text = node.get("TEXT")
-
-    if text[0:2] == "tp":
-        print("--------------------> 前置条件:\n " + text)
-        one_row[4] = text[3:]
-        return
-    elif text[0:2] == "tr":
-        print("--------------------> 备注:\n " + text)
-        one_row[8] = text[3:]
-        return
-    elif text[0:2] == "te":
-        print("--------------------> 异常情况说明:\n " + text)
-        one_row[9] = text[3:]
-        return
-    elif len(text) == 2 and text[0:1] == "P":
-        print("--------------------> 优先级:\n " + text)
-        one_row[10] = text
-        return 
-    else:
-        print("--------------------> level "+str(level)+" :\n " + text)
-        one_row[level] = text
-
-    # 递归下一层级
-    for sub_node in node:
-        deal_mm_file(sub_node, level+1)
-    # 添加一行表格
-    if level == 6:
-        addNewNode2()
-        # 清空最后三行的数据
-        one_row[7] = ''
-        one_row[8] = ''
-        one_row[9] = ''
-        one_row[10] = ''
-
-    # 清空本次递归填写的数据
-    if level != 7:
-        one_row[level] = ''
-
-def addNewNode2():
-    print("--------------------> 正在输出一行表格 <--------------------")
-    sheet.append([one_row[0].strip(), one_row[1].strip(), one_row[2].strip(), one_row[3].strip(), one_row[4].strip(), "\n"+one_row[5].strip()+"\n", one_row[6].strip(), one_row[7].strip(), one_row[8].strip(), one_row[9].strip(), one_row[10].strip()])
-                    
-
-
-
-# 将上面复杂的代码, 转换为一个递归进行处理
-# 递归中包含这样的逻辑: 
-#   当此节点的子节点长度为0时,代表此节点是最后一个了, 此时结束循环.
-
-one_row = ['', '', '', '', '', '', '', '', '', '', '']
-# 测试主模块
-for test_main_module in top_node:
-    if test_main_module.tag != "node":
-        continue
-    deal_mm_file(test_main_module, 1)
-    # 清空
-    one_row = ['', '', '', '', '', '', '', '', '', '', '']
-
-
-
-# 表格生成完成，进行格式处理
-thin = Side(border_style="thin", color="000000")
-
-i = 1
-# 对所有单元格进行处理(横排竖排对齐, 以及边框)
-while sheet.cell(i, 1).value != None:
-    for j in range(1, 12):
-        if i== 1:
-            # 上下左右居中对齐，自动换行
-            sheet.cell(i, j).alignment = Alignment(horizontal='center', vertical='center', wrapText=True)
-        elif j == 1 or j == 2 or j == 3 or j == 4:
-            sheet.cell(i, j).alignment = Alignment(horizontal='center', vertical='center', wrapText=True)
-        else:
-            sheet.cell(i, j).alignment = Alignment(horizontal='left', vertical='center', wrapText=True)
-        # 边框
-        sheet.cell(i, j).border = Border(top=thin, left=thin, right=thin, bottom=thin)
-    i += 1
-
-# 设置背景颜色和加粗
-k = 1
-ft = Font(bold=True)
-while sheet.cell(1, k).value != None:
-    sheet.cell(1, k).font = ft
-    sheet.cell(1, k).fill = PatternFill("solid", fgColor="5b9bd5")
-    k+=1
-
-# 对2-5列的表格进行相邻行内容相同归并处理
-for col in range (2, 6):
-    row = 2
-    start_row = 2
-    end_row = start_row
-    start_value = sheet.cell(row, col).value 
-    #if start_value == None or start_value == "":
-    #    continue
-    while sheet.cell(row+1, col).value != None:
-        print(str(row+1) +", " + str(col))
-        if sheet.cell(row + 1, col).value == start_value and start_value != "":
-            end_row = row + 1
-        else:
-            sheet.merge_cells(start_row=start_row, start_column=col, end_row=end_row, end_column=col)
-            start_row = row + 1
-            end_row = start_row
-            start_value = sheet.cell(start_row, col).value
-        row += 1
-    sheet.merge_cells(start_row=start_row, start_column=col, end_row=end_row, end_column=col)
+# 如果获取到0, 则输出根节点下所有包含测试用例的子节点作为起点, 输出多个sheet工作簿
+if sheet_name == "0":
+    top_node = root.find("./node[@TEXT]")
+    count = 0
+    for sub_node in top_node:
+        if sub_node.tag != "node":
+            continue
+        if sub_node.get("TEXT").find("测试用例") != -1:
+            if count == 0:
+                sheet = wb.active
+                sheet.title = sub_node.get("TEXT")
+            else:
+                sheet = wb.create_sheet(sub_node.get("TEXT"), count)
+            create_testcase_sheet(sheet, sub_node);
+            count = count + 1
 
 
 # 需要使用正则表达式，匹配文件名，并复制给新文件
